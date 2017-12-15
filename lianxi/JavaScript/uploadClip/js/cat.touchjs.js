81aa9d608d8075129bceab074cac221995190a73
L-WindRider <1693067201@qq.com>
2017-12-15 12:19:35 +0800

    上传图片完成

--- lianxi/JavaScript/uploadClip/js/cat.touchjs.js
+++ lianxi/JavaScript/uploadClip/js/cat.touchjs.js
@@ -8,8 +8,9 @@ cat.touchjs = {
     scaleVal: 1,    //缩放
     rotateVal: 0,   //旋转
     curStatus: 0,   //记录当前手势的状态, 0:拖动, 1:缩放, 2:旋转
-    ydx:0,//移动X
-    ydy:0,
+    ydx:0,//记录移动X坐标
+    ydy:0,//记录Y坐标
+    deviation:0,//定义图片居中偏移值
     //初始化
     init: function (clipDivCover,clipImg, callback) {
         touch.on(clipDivCover, 'touchstart', function (ev) {
@@ -37,19 +38,31 @@ cat.touchjs = {
 
     //拖动
     drag: function (clipDivCover,clipImg, callback) {
-        console.log("拖动");
         touch.on(clipDivCover, 'drag', function (ev) {
             cat.touchjs.ydx=cat.touchjs.left + ev.x;
             cat.touchjs.ydy=cat.touchjs.top + ev.y;
             console.log(cat.touchjs.left,ev.x,cat.touchjs.top,ev.y,ev);
-            var continu=cat.touchjs.Continue(ev);
-            if(continu){
-                clipImg.css("left", cat.touchjs.ydx).css("top", cat.touchjs.ydy);
-            }else{
+            if(cat.touchjs.ydx>circleX+(clipImgW*cat.touchjs.scaleVal-clipImgW)/2 && ev.x>0){
                 cat.touchjs.ydx=cat.touchjs.left;
                 cat.touchjs.ydy=cat.touchjs.top;
+                return;
             }
-            // clipImg.css("left", ydx).css("top", ydy);
+            else if(cat.touchjs.ydy>circleY+(clipImgH*cat.touchjs.scaleVal-clipImgH)/2 && ev.y>0){
+                cat.touchjs.ydx=cat.touchjs.left;
+                cat.touchjs.ydy=cat.touchjs.top;
+                return;
+            }
+            else if(cat.touchjs.ydx < circleX + circleRadius * 2-(clipImgW*cat.touchjs.scaleVal-clipImgW)/2-clipImgW&&ev.x<0){
+                cat.touchjs.ydx=cat.touchjs.left;
+                cat.touchjs.ydy=cat.touchjs.top;
+                return;
+            }
+            else if(cat.touchjs.ydy < circleY + circleRadius * 2-(clipImgH*cat.touchjs.scaleVal-clipImgH)/2-clipImgH&&ev.y<0){
+                cat.touchjs.ydx=cat.touchjs.left;
+                cat.touchjs.ydy=cat.touchjs.top;
+                return;
+            }
+            clipImg.css("left", cat.touchjs.ydx).css("top", cat.touchjs.ydy);
         });
         touch.on(clipDivCover, 'dragend', function (ev) {
              cat.touchjs.left =cat.touchjs.ydx;
@@ -68,6 +81,18 @@ cat.touchjs = {
             cat.touchjs.curStatus = 1;
             currentScale = ev.scale - 1;
             currentScale = initialScale + currentScale;
+            if(cat.touchjs.ydx>circleX+(clipImgW*cat.touchjs.scaleVal-clipImgW)/2&&cat.touchjs.scaleVal>currentScale){
+                return;
+            }
+            else if(cat.touchjs.ydy>circleY+(clipImgH*cat.touchjs.scaleVal-clipImgH)/2&&cat.touchjs.scaleVal>currentScale){
+                return;
+            }
+            else if(cat.touchjs.ydx < circleX + circleRadius * 2-(clipImgW*cat.touchjs.scaleVal-clipImgW)/2-clipImgW&&cat.touchjs.scaleVal>currentScale){
+                return;
+            }
+            else if(cat.touchjs.ydy < circleY + circleRadius * 2-(clipImgH*cat.touchjs.scaleVal-clipImgH)/2-clipImgH&&cat.touchjs.scaleVal>currentScale){
+                return;
+            }
             cat.touchjs.scaleVal = currentScale;
             var transformStyle = 'scale(' + cat.touchjs.scaleVal + ')';
             clipImg.css("transform", transformStyle).css("-webkit-transform", transformStyle);
